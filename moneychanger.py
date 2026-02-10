30f9380cfeb90073f8808526cfd7a09406b41f99
Sourish Dey <142993520+sourish0903@users.noreply.github.com>
2026-02-10 10:11:34 +0000

    modified with conditional tool call

--- moneychanger.py
+++ moneychanger.py
@@ -10,6 +10,13 @@ from openai import OpenAI
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 EXCHANGERATE_API_KEY = os.getenv('EXCHANGERATE_API_KEY')
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "gpt-4o-mini"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=GITHUB_TOKEN,
+)
 def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
     """Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
     load_dotenv()
@@ -22,80 +29,81 @@ def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
-    functions = [
-        {
-            "name": "get_exchange_rate",
-            "description": "Get current exchange rate value using the function arguments from https://v6.exchangerate-api.com/v6.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "base": {
-                        "type": "string",
-                        "description": "Base currency(e.g. 'GBP'.),the currency the prompt asking to convert. eg. in a prompt convert 100 GBP to to USD GBP is base(Base currency)",
-                    },
-                    "target": {
-                        "type": "string",
-                        "description": "target currency code representing the currency into which the base amount will be converted. three-letter currency code supported by the ExchangeRate API (e.g., 'USD', 'INR', 'GBP') eg. in a prompt convert 100 GBP to to USD USD is target currency",
-                    },
-                    "amount": {
-                        "type": "string",
-                        "description": "The amount in the base currency to be converted.",
-                    },
-                },
-                "required": ["base", "target", "amount"],
-            },
-        },
-    ]
     
-    try: 
-        endpoint = "https://models.github.ai/inference"
-        model_name = "openai/gpt-4o-mini"
+    tools = [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "get_exchange_rate",
+                        "description": "Convert a given amount of money from one currency to another. Each currency will be represented as a 3-letter code",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "base": {
+                                    "type": "string",
+                                    "description": "The base or original currency.",
+                                },
+                                "target": {
+                                    "type": "string",
+                                    "description": "The target or converted currency",
+                                },
+                                "amount": {
+                                    "type": "string",
+                                    "description": "The amount of money to convert from the base currency.",
+                                },
+                            },
+                            "required": ["base", "target", "amount"],
+                            "additionalProperties": False,
+                        },
+                    },
+                }
+                ]
 
-        client = OpenAI(
-            base_url=endpoint,
-            api_key=GITHUB_TOKEN,
-        )
-
+    try:
         response = client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a helpful assistant",
+                    "content": "You are a helpful assistant.",
                 },
                 {
                     "role": "user",
-                    "content": f"{textbox_input}",
+                    "content": textbox_input,
                 }
             ],
             temperature=1.0,
             top_p=1.0,
             max_tokens=1000,
             model=model_name,
-            functions=functions,
+            tools=tools,
+            # function_call="auto"
         )
 
-        # answer = response.choices[0].message.content
-        return response
+    
     except Exception as e:
         print(f"Exception {e} for {textbox_input}")
+    else:
+        return response#.choices[0].message.content
 
 
 def run_pipeline(textbox_input):
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
     Call get_exchange_rate(...) if necessary"""
 
-    # 1. Define a list of callable tools for the mode
+    response = call_llm(textbox_input)
     
-    # if answer == 'True': #tool_calls
-        
-    #     get_exchange_rate()
-    #     st.write(f'{base} {amount} is {target} {exchange_response["conversion_result"]:.2f}')
+    if response.choices[0].finish_reason == 'tool_calls':
+        response_arument = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+        base = response_arument['base']
+        target = response_arument['target']
+        amount = response_arument['amount']
+        _, _, _, conversion_result = get_exchange_rate(base, target, amount)
+        st.write(f'{base} {amount} is {target} {conversion_result}')
 
-    # elif True: #tools not used
-    #     # Update this
-    #     st.write(f"(Function calling not used) and response from the model")
-    # else:
-    #     st.write("NotImplemented")
+    elif response.choices[0].finish_reason == 'stop':
+        st.write(f"(Function calling not used) and response from the model {response.choices[0].message.content}")
+    else:
+        st.write("NotImplemented")
 
  # Title of the app
 st.title("Multilingual Money Changer")
@@ -106,4 +114,4 @@ user_input = st.text_input("Enter the amount and the currency")
 # Submit button
 if st.button("Submit"):
     # Display the input text below the text box
-    st.write(call_llm(user_input)) 
+    st.write(run_pipeline(user_input)) 
